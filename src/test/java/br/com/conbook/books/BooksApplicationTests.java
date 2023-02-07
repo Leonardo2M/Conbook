6f79cba8aff4c90eca34ea045eef781139cb7c8e
Leonardo Matheus <leonardomm.contato@gmail.com>
2023-02-07 15:40:01 -0300

    Creating project

---
+++ src/test/java/br/com/conbook/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.conbook.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
